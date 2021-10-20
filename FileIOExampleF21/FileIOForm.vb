9f9fb5b4341245b290719df73aafde6bf0a0cec0
rosstimo <rosstimo@isu.edu>
2021-10-20 08:53:30 -0600

    Add project files.

---
+++ FileIOExampleF21/FileIOForm.vb
@@ -0,0 +1,21 @@
+﻿Public Class FileIOForm
+    Private Sub FileIOForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        WriteFile()
+    End Sub
+
+    Sub WriteFile()
+        'absolute path
+        'Dim fileName As String = "C:\Users\rosstimo\OneDrive\Sync\github\FileIOExampleF21\exampleF21.txt"
+
+        'relative path: up two dir levels
+        Dim fileName As String = "../../exampleF21.txt"
+
+        FileOpen(1, fileName, OpenMode.Output)
+
+        FileClose(1)
+
+    End Sub
+
+
+
+End Class
