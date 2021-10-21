2efcc29819b42f28f041a3a2469d2cc185845495
rosstimo <rosstimo@isu.edu>
2021-10-21 08:52:23 -0600

    add append and read examples

--- FileIOExampleF21/FileIOForm.vb
+++ FileIOExampleF21/FileIOForm.vb
@@ -1,6 +1,9 @@
 ﻿Public Class FileIOForm
     Private Sub FileIOForm_Load(sender As Object, e As EventArgs) Handles Me.Load
-        WriteFile()
+        'WriteFile()
+        'AppendFile()
+        ReadRecords()
+        Me.Close()
     End Sub
 
     Sub WriteFile()
@@ -10,12 +13,60 @@
         'relative path: up two dir levels
         Dim fileName As String = "../../exampleF21.txt"
 
+        'Open file for write access
+        'destroys existing file contents
         FileOpen(1, fileName, OpenMode.Output)
-
+        ' Print text to the file. The quotation marks will be in the display.
+        Write(1, "Follow the white rabbit...")
         FileClose(1)
 
     End Sub
 
 
+    Sub AppendFile()
+        Dim fileName As String = "../../exampleF21.txt"
+
+        'Append to the end of file
+        'preserves existing file content
+        FileOpen(1, fileName, OpenMode.Append)
+
+        'Write(1, "Follow the white rabbit...")
+        'WriteLine(1)'write only a new line delimiter
+        'WriteLine(1, "Knock Knock Neo....")
+        WriteLine(1, "Zone 1", SPC(10), "Zone 2")
+        WriteLine(1, "Zone 1", StrDup(10, " "), "Zone 2")
+
+        FileClose(1)
+
+    End Sub
+
+    Sub ReadLines()
+
+    End Sub
+
+    Sub ReadRecords()
+        Dim fileName As String = "../../exampleF21.txt"
+        Dim recordData As String
+
+        Try
+            FileOpen(1, fileName, OpenMode.Input)
+
+            For i = 0 To 14
+                Input(1, recordData)
+                Console.WriteLine(recordData)
+            Next
+            Do Until EOF(1)
+                Input(1, recordData)
+                Console.WriteLine(recordData)
+            Loop
+
+        Catch notFound As System.IO.FileNotFoundException
+            MsgBox("oops that file doesn't exist")
+        Catch e As Exception
+            MsgBox("that was weird!")
+        End Try
+
+        FileClose(1)
+    End Sub
 
 End Class
